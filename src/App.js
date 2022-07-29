dcc2f91d52fc2682ec03c23430d8adafe1b2f8d1
John F <jkforsythe@hotmail.com>
2022-07-29 17:40:15 +0100

    Created expense component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 const expenses = [
   {
@@ -26,7 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      
+      <Expenses items={expenses}/>
     </div>
   );
 }
